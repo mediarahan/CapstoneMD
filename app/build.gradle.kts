4c31358be4718ac25e7724f0d77eee7b3573c946
Achmad Rionov <72985189+mediarahan@users.noreply.github.com>
2023-12-18 19:12:28 +0700

    Added functions for fetching device data for API purposees

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,4 +74,8 @@ dependencies {
     //datastore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    //maps
+    implementation("com.google.android.gms:play-services-maps:18.0.0")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
+
 }
--- app/src/main/java/com/dicoding/capstonemd/ui/detail/TabMapsFragment.kt
+++ app/src/main/java/com/dicoding/capstonemd/ui/detail/TabMapsFragment.kt
@@ -1,18 +1,99 @@
 package com.dicoding.capstonemd.ui.detail
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dicoding.capstonemd.R
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.dicoding.capstonemd.databinding.FragmentTabMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class TabMapsFragment : Fragment() {
+
+    private lateinit var binding: FragmentTabMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+    //temporary
+    private lateinit var locationTextView: TextView
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions[Manifest.permission.ACCESS_FINE_LOCATION] ?: false -> {
+                    // Precise location access granted.
+                    getMyLastLocation()
+                }
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] ?: false -> {
+                    // Only approximate location access granted.
+                    getMyLastLocation()
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_tab_maps, container, false)
+    ): View {
+        binding = FragmentTabMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private fun checkPermission(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getMyLastLocation() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION) &&
+            checkPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (location != null) {
+                    // Handle the location data (latitude and longitude)
+                    handleLocationData(location.latitude, location.longitude)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Location is not found. Try Again",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        } else {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+
+    private fun handleLocationData(latitude: Double, longitude: Double) {
+        val locationText = "Latitude: $latitude, Longitude: $longitude"
+        locationTextView.text = locationText
+    }
+
 }
