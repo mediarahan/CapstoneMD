2199e2a5f17e0231baed0addb9f238c1b66ed59e
Achmad Rionov <72985189+mediarahan@users.noreply.github.com>
2023-12-21 19:59:18 +0700

    initial commit for ml implementation branch.

---
+++ app/src/main/java/com/dicoding/capstonemd/ui/question/QuestionActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.capstonemd.ui.question
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.capstonemd.R
+
+class QuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+    }
+}
