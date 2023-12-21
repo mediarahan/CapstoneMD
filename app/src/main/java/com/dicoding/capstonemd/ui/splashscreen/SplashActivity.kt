726df9f5b557b67f7857676301fee65d6d430737
4c1hh <ahmadzarkasyiilinggau2019@gmail.com>
2023-12-22 04:06:32 +0700

    commit 15-12-23
    - Added bug fix where app keeps starting from the RegisterActivity even though they have logged in
    - Added bug fix where app starts in MainActivity even though they haven't logged in
    - Changed rv design
    - Changed reward pictures to food pictures

---
+++ app/src/main/java/com/dicoding/capstonemd/ui/splashscreen/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.capstonemd.ui.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.dicoding.capstonemd.R
+import com.dicoding.capstonemd.ui.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}
